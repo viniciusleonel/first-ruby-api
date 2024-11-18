9a4155c4f3ab107b97fb6adc121cc98f895776f7
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-11-18 15:21:25 -0300

    fix: corrigindo processamento do arquivo

--- src/file_processor.rb
+++ src/file_processor.rb
@@ -1,31 +1,52 @@
 require 'pg'
 require 'date'
+require 'async'
 
 class FileProcessor
+  def self.save_data_to_db
+    connection = Database.connect
 
-  def save_data_to_db(file_path)
-    begin
-      connection = Database.connect
+    Async do
+      total_lines = `wc -l < challenge/data_1.txt`.to_i # Conta o número total de linhas no arquivo
+      current_line = 0 # Contador para o progresso
 
-      File.foreach(file_path) do |line|
-        # Exemplo de como você pode separar os dados e construir a query
-        user_id, name, total, date = parse_line(line) # Adapte para o formato dos seus dados
-        connection.exec_params("INSERT INTO users (user_id, name) VALUES ($1, $2)", [user_id, name])
+      # Ler o arquivo
+      File.foreach('challenge/data_1.txt') do |linha|
+        user_id = linha[0, 10].strip.to_i
+        name = linha[10, 45].strip
+        order_id = linha[55, 10].strip.to_i
+        product_id = linha[65, 10].strip.to_i
+        value = linha[75, 12].strip.to_f
+        date = Date.strptime(linha[87, 8].strip, '%Y%m%d')
 
-        # Inserir dados em 'orders' e 'products' conforme necessário
-        order_id, value, product_id = parse_order_and_product(line)
-        connection.exec_params("INSERT INTO orders (order_id, user_id, total, date) VALUES ($1, $2, $3, $4)",
-                               [order_id, user_id, total, date])
+        # Salvar o usuário
+        connection.exec_params(
+          "INSERT INTO users (user_id, name) VALUES ($1, $2) ON CONFLICT (user_id) DO NOTHING",
+          [user_id, name]
+        )
 
-        connection.exec_params("INSERT INTO products (product_id, order_id, value) VALUES ($1, $2, $3)",
-                               [product_id, order_id, value])
+        # Salvar o pedido
+        connection.exec_params(
+          "INSERT INTO orders (order_id, user_id, total, date) VALUES ($1, $2, $3, $4) ON CONFLICT (order_id) DO NOTHING",
+          [order_id, user_id, value, date]
+        )
+
+        # Salvar o produto
+        connection.exec_params(
+          "INSERT INTO products (product_id, order_id, value) VALUES ($1, $2, $3) ON CONFLICT (product_id) DO NOTHING",
+          [product_id, order_id, value]
+        )
+
+        current_line += 1
+
+        # Atualizar o progresso a cada 100 linhas processadas
+        if current_line % 100 == 0
+          puts "Processando linha #{current_line} de #{total_lines}..."
+        end
       end
-
-    rescue PG::Error => e
-      puts "Erro ao processar os dados: #{e.message}"
+      puts "Processamento completo!"
     ensure
       connection.close if connection
     end
   end
-
 end
