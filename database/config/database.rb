e4707377424975c071731914765005949d3232d7
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-11-24 07:28:12 -0300

    feat: exibindo mensagem de erro de falha de conexao com database

--- database/config/database.rb
+++ database/config/database.rb
@@ -3,18 +3,24 @@ require 'dotenv/load'
 
 class Database
   def self.connect
-    case ENV['PROFILE']
-    when 'test'
-      db_test_url = ENV['DATABASE_TEST_URL']
-      PG.connect(db_test_url)
-    when 'development'
-      db_local_url = ENV['DATABASE_LOCAL_URL']
-      PG.connect(db_local_url)
-    when 'production'
-      db_prod_url = ENV['DATABASE_PROD_URL']
-      PG.connect(db_prod_url)
-    else
-      raise "Unknown PROFILE: #{ENV['PROFILE']}"
+    begin
+      case ENV['PROFILE']
+      when 'test'
+        db_test_url = ENV['DATABASE_TEST_URL']
+        PG.connect(db_test_url)
+      when 'development'
+        db_local_url = ENV['DATABASE_LOCAL_URL']
+        PG.connect(db_local_url)
+      when 'production'
+        db_prod_url = ENV['DATABASE_PROD_URL']
+        PG.connect(db_prod_url)
+      else
+        raise "Unknown PROFILE: #{ENV['PROFILE']}"
+      end
+    rescue PG::Error => e
+      puts "Não foi possível estabelecer a conexão com o banco de dados. Verifique os links de conexão e o perfil de desenvolvimento."
+      puts "Erro: #{e.message}"
+      nil
     end
   end
 end
